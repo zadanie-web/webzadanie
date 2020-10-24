aea3913c87b26c86c8e0bc5865e1d173b4107061
web <web@mkbooksystems.sk>
2020-10-24 09:35:43 +0200

    Link config and create simple routes

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+include_once 'Config/Config.php';
+$routes = [
+    '/' => [
+       '',
+    ],
+];
+
+$page = segment(1);
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ( ! isset( $routes["/$page"][$method]) ) {
+    show_404();
+}
+
+
+require $routes["/$page"][$method] ;
