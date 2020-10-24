1ba3417e23170dfa15e4d613cbebc014c2ae7988
web <web@mkbooksystems.sk>
2020-10-24 19:18:42 +0200

    ALL HOME PAGE

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 include_once 'Config/Config.php';
 include_once 'Partials/Header.php';
+
 $routes = [
     '/' => [
-       '',
+       'GET' => 'Partials/Body.php',
+        'POST' => 'Partials/Body.php',
     ],
 ];
-include_once 'Partials/Footer.php';
+
+
+
+
+
+
 
 $page = segment(1);
 $method = $_SERVER['REQUEST_METHOD'];
@@ -14,6 +21,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ( ! isset( $routes["/$page"][$method]) ) {
     show_404();
 }
-
+include_once 'Partials/Footer.php';
 
 require $routes["/$page"][$method] ;
