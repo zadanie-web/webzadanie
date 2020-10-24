914593273677a568c376d7d0e2df397c79429c70
web <web@mkbooksystems.sk>
2020-10-24 19:09:39 +0200

    Add background image

--- Partials/Body.php
+++ Partials/Body.php
@@ -1,1 +1,5 @@
-<p>Body</p>
+<div class="container-fluid p-0">
+    <div class="headerimage">
+        <img src="<?= packageFille('Media/image.jpeg');?>" alt="Obrázok kategórie" class="rounded w-100 img-fluid">
+    </div>
+</div>
