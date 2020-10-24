4308a68076b226c457fb3f43d96296dbbf53f7bf
web <web@mkbooksystems.sk>
2020-10-24 19:14:24 +0200

    Card add

--- Partials/Body.php
+++ Partials/Body.php
@@ -2,4 +2,22 @@
     <div class="headerimage">
         <img src="<?= packageFille('Media/image.jpeg');?>" alt="Obrázok kategórie" class="rounded w-100 img-fluid">
     </div>
+    <div class="position-absolute mycon">
+        <div class="container">
+            <h2 class="white bold">FOTOGALÉRIA</h2>
+            <h3 class="white bold mt-my">KATEGÓRIE</h3>
+            <hr>
+            <div class="card-deck">
+                <div class=" mt-4">
+                    <div class="card">
+                        <img class="card-img-top" src="<?= packageFille('Media/'.'p'.$i.'x.jpeg');?>" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="text-center bold  m-0 card-title">Architektúra</p>
+                            <p class="card-text mycall text-center">6 fotiek</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
