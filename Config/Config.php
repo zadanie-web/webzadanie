34190dff0eaf7aeffec8cbd0c1a8ace89a5dd948
web <web@mkbooksystems.sk>
2020-10-24 09:28:28 +0200

    Link my function

--- Config/Config.php
+++ Config/Config.php
@@ -1,1 +1,12 @@
 <?php
+require_once 'Function/FunctionLink.php';
+
+
+/** FOR DATABASE */
+
+
+
+
+/** my function */
+require_once 'Function/FunctionGeneral.php';
+require_once 'Function/FunctionFormat.php';
