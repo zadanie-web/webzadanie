9249fbbd32c3af6adb9fe046a582463c50094407
web <web@mkbooksystems.sk>
2020-10-24 09:30:51 +0200

    Add html and link css

--- Partials/Header.php
+++ Partials/Header.php
@@ -1,1 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Zadanie</title>
+    <?php  // MY CSS AUTOCONNECT
+      add_css('Access','Css','*.css');
+    ?>
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+
+
+</head>
+<body>
