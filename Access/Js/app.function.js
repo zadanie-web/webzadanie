e094a8d548ca43365b473d27d945e6fab44ce71f
web <web@mkbooksystems.sk>
2020-10-25 12:11:54 +0100

    Add function center

--- Access/Js/app.function.js
+++ Access/Js/app.function.js
@@ -1,8 +1,32 @@
+/**
+ * FUNCTION CENTER
+ * POSITION (absolute)
+ * HEIGHT , WIDTH 1/3 and 1/2
+ */
+(function($){
+    $.fn.extend({
+        center: function () {
+            return this.each(function() {
+                //let tops = $(this).attr("data-top");
+                let top = ($(window).height() - $(this).outerHeight()) / 3;
+                let left = ($(window).width() - $(this).outerWidth()) / 2;
+                $(this).css({position:'absolute', margin:0, top: (top > 0 ? top : 0)+'px', left: (left > 0 ? left : 0)+'px'});
+            });
+        }
+    });
+})(jQuery);
 
 
 
+
+/**
+ * Load kategory start get page
+ */
 datakategory();
 
+/**
+ * FUNCTION FOR LOAD KATEGORY
+ */
 function datakategory(){
     $.ajax({
         url:'Kategory',
