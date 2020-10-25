264112450d60fe88fcbff2a6ef44a4ce4bd8aafc
web <web@mkbooksystems.sk>
2020-10-25 12:11:04 +0100

    Add id for strart dialog

--- Actions/Ajax/Kategory.php
+++ Actions/Ajax/Kategory.php
@@ -11,7 +11,7 @@
         </div>
     <?php endfor; ?>
     <div class="mt-4">
-        <div class="card card-block">
+        <div id="openkategory" class="card card-block">
             <div class="card-body text-center mt-5">
                 <img src="<?= packageFille('Media/a.svg');?>" width="40" alt="ikonka add" class="filter-green">
                 <p class="mt-2 bold darkgrey">PRIDAŤ KATEGÓRIU</p>
