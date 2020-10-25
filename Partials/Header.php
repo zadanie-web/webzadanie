b44aa54868f10a6e2b21ffdd96fcf0e7f9ff7d6e
web <web@mkbooksystems.sk>
2020-10-25 11:28:37 +0100

    Pridanie jquery

--- Partials/Header.php
+++ Partials/Header.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Zadanie</title>
+    <link rel="stylesheet" href="<?= packageFille('Package/ui/jquery-ui.min.css');?>">
+    <script type="javascript" src="<?= packageFille('Package/jquery/jquery.js');?>"></script>
+    <script type="javascript" src="<?= packageFille('Package/ui/jquery-ui.js');?>"></script>
     <?php  // MY CSS AUTOCONNECT
       add_css('Access','Css','*.css');
     ?>
